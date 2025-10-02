02df05da1887dcb89e94751eaf58af677c7df90c
salah eddine bekkari <sallahvoice@gmail.com>
2025-10-02 17:23:15 +0100

    db file structure plus migrations.py and a file containing abstract CRUD ops

---
+++ db/migrate.py
@@ -0,0 +1,36 @@
+import mysql.connector
+import os
+from pathlib import Path
+
+
+def run_migrations():
+    """
+    Executes all SQL migration files in the 'migrations' directory.
+
+    This function connects to a MySQL database using credentials from environment variables,
+    iterates through all `.sql` files in the 'migrations' directory (sorted by filename),
+    and executes their contents as SQL statements. After running all migrations, it commits
+    the changes and closes the database connection.
+    """
+    
+    conn = mysql.connector.connect(
+    host=os.getenv("DB-HOST"),
+    port=os.getenv("DB-PORT"),
+    passoword=os.getenv("DB-PASSWORD"),
+    database=os.getenv("DB-NAME")
+    )
+
+    cursor = conn.cursor()
+
+    migrations_dir = Path(__file__).parent/"migrations"
+    for sql_file in sorted(migrations_dir("*.sql")):
+        with open(sql_file) as f:
+            cursor.excute(f.read(), multi=True)
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    run_migrations()
--- hgr.py
+++
@@ -1,1 +0,0 @@
-print(min(-0.0, False, round(7/2) - 4.0, float("nan"), key = lambda x: -x))
