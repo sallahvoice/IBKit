b4914d5070984bb3de6ca60e5fdfe9ccbb5d1ba1
sallahvoice <sallahvoice@gmail.com>
2025-10-25 11:15:00 +0100

    simple test file to test service layer

---
+++ tests/service_test.py
@@ -0,0 +1,18 @@
+from backend.services.comparables_service import analyze_company
+
+def analyze_apple():
+    result = analyze_company("APPL")
+
+    if "error" in result:
+        print(f"❌ error: {result['error']}")
+    else:
+        print("✅ success")
+        print(f"found {result['comparable_count']} comparables")
+        print(f"list of comparables: {result['comparables']}")
+        print(f"summary of stats {result['summary_stats']}")
+
+    return result
+
+
+if __name__ == "__main__":
+    analyze_apple()
